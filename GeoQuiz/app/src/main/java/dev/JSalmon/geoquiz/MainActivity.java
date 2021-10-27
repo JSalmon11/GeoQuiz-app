f8e3d45d0afaf61e00be774d2a0bc607f5b012bd
Jorge Salmon <kokesalmon@gmail.com>
2021-10-27 08:01:18 +0200

    commit an casa no funka

--- GeoQuiz/app/src/main/java/dev/JSalmon/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/dev/JSalmon/geoquiz/MainActivity.java
@@ -2,18 +2,30 @@ package dev.JSalmon.geoquiz;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-//VOY POR LA PAGINA 64 DEL NERDS 2
+
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView preguntasTextView;
+    private Preguntas[] arrayPreguntas=new Preguntas[]{
+            new Preguntas(R.string.pregunta1, true),
+            new Preguntas(R.string.pregunta2, false),
+            new Preguntas(R.string.pregunta3, true),
+    };
+    private int currentIndex=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preguntasTextView= findViewById(R.id.textPreguntas);
+        int pregunta=arrayPreguntas [currentIndex].getTextResId();
+        preguntasTextView.setText(pregunta);
 
         mTrueButton= findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(v -> Toast.makeText(MainActivity.this,
